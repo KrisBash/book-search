a9b7272519a3f01344aa279957ee31c095d5bbd8
Kristopher Bash <krbash@microsoft.com>
2018-02-23 13:55:50 -0800

    update environment playbooks

--- BuildEnv/Infra/gitlab_runner.sh
+++ BuildEnv/Infra/gitlab_runner.sh
@@ -1,5 +1,5 @@
 curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | bash && apt-get update && apt-get install gitlab-runner
 host_name=`hostname`
-gitlab-runner register -u https://gitlab.com/ -r $gitlab_token --executor shell --taglist linux -n  
+gitlab-runner register -n  -u https://gitlab.com/ -r $gitlab_token --executor shell --tag-list linux
 echo "gitlab-runner ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/gitlab
 gitlab-runner start
