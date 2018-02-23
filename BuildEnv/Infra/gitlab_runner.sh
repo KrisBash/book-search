ed96ac574374162c00a867a571d9e8f22fccbae0
Kristopher Bash <krbash@microsoft.com>
2018-02-23 11:23:55 -0800

    update buildNode.yml

--- BuildEnv/Infra/gitlab_runner.sh
+++ BuildEnv/Infra/gitlab_runner.sh
@@ -1,5 +1,5 @@
 curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | bash && apt-get update && apt-get install gitlab-runner
 host_name=`hostname`
-gitlab-runner register -u https://gitlab.com/ -r $gitlab_token --executor shell  -n  
+gitlab-runner register -u https://gitlab.com/ -r $gitlab_token --executor shell --taglist linux -n  
 echo "gitlab-runner ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/gitlab
 gitlab-runner start
