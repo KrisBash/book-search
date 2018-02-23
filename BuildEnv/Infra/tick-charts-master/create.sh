ac26ffbce0768b37e4b13a36dc65f4e48cd910e0
Kristopher Bash <krbash@microsoft.com>
2018-02-23 10:58:43 -0800

    update infra scripts and statsd server

--- BuildEnv/Infra/tick-charts-master/create.sh
+++ BuildEnv/Infra/tick-charts-master/create.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-
+do#!/bin/bash
+#helm package influxdb chronograf telegraf kapacitor
 helm install --name data --namespace tick ./influxdb/
 helm install --name polling --namespace tick ./telegraf-s/
 helm install --name hosts --namespace tick ./telegraf-ds/
 helm install --name alerts --namespace tick ./kapacitor/
 helm install --name dash --namespace tick ./chronograf/
-kkubectl get svc -w --namespace tick -l app=dash-chronograf
+kubectl get svc -w --namespace tick -l app=dash-chronograf
