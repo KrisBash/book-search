49b84b02efd5f09405c90779cf1660cf567440d2
Kristopher Bash <krbash@microsoft.com>
2018-02-23 11:05:44 -0800

    add -y to ansible install

--- BuildEnv/initialize.sh
+++ BuildEnv/initialize.sh
@@ -18,7 +18,7 @@ then
     sudo apt-get install software-properties-common
     sudo apt-add-repository ppa:ansible/ansible
     sudo apt-get update
-    sudo apt-get install ansible
+    sudo apt-get install -y ansible
 
 fi
 cp ./BuildEnv/initialize.sh /tmp/initialize.sh 
