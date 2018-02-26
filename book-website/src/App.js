e4a24f41cad86490b05d7f46e037d8df64c0ab9f
Kristopher Bash <krbash@microsoft.com>
2018-02-26 09:21:40 -0800

    use env variables for API address

--- book-website/src/App.js
+++ book-website/src/App.js
@@ -53,7 +53,7 @@ export class BookForm extends React.Component {
   }
 
   search_for_book(isbn) {
-    this.url_root = `http://book-api:8222/graphql?query={book(isbn:"` + isbn + `"){isbn,`;
+    this.url_root = `http://` + api_uri +`/graphql?query={book(isbn:"` + isbn + `"){isbn,`;
     this.fields = "";
     this.url_tail = `}}`;
     if (this.state.cb_book_authors){this.fields += "authors,"};
@@ -209,6 +209,7 @@ class Header extends Component {
 class App extends Component {
 
   componentDidMount() {
+    const api_uri = `${BOOK_API_SERVICE_HOST}:${BOOK_API_SERVICE_PORT}`
   }
   render() {
     return (
